e061b0027e61c878789f92c07e2642b56eea9ba3
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-04-05 02:32:18 +0530

    again

--- bankAccount.js
+++ bankAccount.js
@@ -1,29 +1,27 @@
 
-
 function createBankAccount(N){
     let balance=N
+
     function deposit(amount){
         balance+=amount
-        return `Deposit of ${amount} successful!`
+        return balance
     }
     function withdraw(amount){
-       if(amount<balance){
-         balance=balance-amount
-        return `Withdrawal of ${amount} successful!`
-       }
-       else{
-        return `Insufficient Fund, available balance:${balance}`
-       }
+         balance-=amount
+        return balance
     }
     function getBalance(){
-        return `Total balance availabe: ${balance}`
+        return balance
     }
     return{deposit,
         withdraw,
         getBalance
     }
 }
-let Bank=createBankAccount(1000)
-console.log(Bank.deposit(400))
-console.log(Bank.withdraw(100))
-console.log(Bank.getBalance())
+const account = createBankAccount(100);
+
+console.log(account.deposit(50)); // Output: 150
+
+console.log(account.withdraw(30)); // Output: 120
+
+console.log(account.getBalance()); // Output: 120
